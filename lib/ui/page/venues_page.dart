5861f18ce7b3147f56818592e2bc159826b3b34c
Daniela Cruz <dccruzt@gmail.com>
2023-04-04 20:43:05 -0300

    chore: refactor venues list view

--- lib/ui/page/venues_page.dart
+++ lib/ui/page/venues_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:venues_per_location/ui/widget/venues_list_view.dart';
 
-import '../../core/design_system/components/action_row.dart';
-import '../../core/design_system/components/custom_divider.dart';
-import '../../core/design_system/spacings.dart';
 import '../controller/venues_controller.dart';
-import '../widget/favorite_button_icon.dart';
 import 'loading_page.dart';
 
 class VenuesPage extends StatelessWidget {
@@ -23,39 +20,10 @@ class VenuesPage extends StatelessWidget {
             final venues = state.venues;
 
             if (venues != null && venues.isNotEmpty) {
-              return ListView.separated(
-                itemBuilder: (context, index) {
-                  final venue = venues[index];
-
-                  return ActionRow(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(x2),
-                      child: Image.network(
-                        venue.imageUrl,
-                        fit: BoxFit.cover,
-                        width: x20,
-                        height: x20,
-                      ),
-                    ),
-                    primary: Text(
-                      venue.name,
-                      maxLines: 2,
-                      style: theme.textTheme.titleMedium,
-                    ),
-                    secondary: Text(venue.description, maxLines: 2),
-                    trailing: FavoriteButtonIcon(
-                      venue.favorite ? Icons.favorite : Icons.favorite_outline,
-                      onPressed: () =>
-                          VenuesCubitProvider.of(context).setFavorite(
-                        id: venue.id,
-                        isFavorite: !venue.favorite,
-                      ),
-                    ),
-                    onTap: () {},
-                  );
-                },
-                separatorBuilder: (context, index) => const CustomDivider(),
-                itemCount: venues.length,
+              return VenuesListView(
+                venues: venues,
+                onPressed: (id, isFavorite) => VenuesCubitProvider.of(context)
+                    .setFavorite(id: id, isFavorite: isFavorite),
               );
             }
             if (state.error != null) {
---
+++ lib/ui/widget/venues_list_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../core/design_system/components/action_row.dart';
+import '../../core/design_system/components/custom_divider.dart';
+import '../../core/design_system/spacings.dart';
+import '../../domain/entity/venue_entity.dart';
+import 'favorite_button_icon.dart';
+
+class VenuesListView extends StatelessWidget {
+  const VenuesListView({
+    Key? key,
+    required this.venues,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final List<VenueEntity> venues;
+  final Function(String id, bool isFavorite) onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        final venue = venues[index];
+
+        return ActionRow(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(x2),
+            child: Image.network(
+              venue.imageUrl,
+              fit: BoxFit.cover,
+              width: x20,
+              height: x20,
+            ),
+          ),
+          primary: Text(
+            venue.name,
+            maxLines: 2,
+            style: theme.textTheme.titleMedium,
+          ),
+          secondary: Text(venue.description, maxLines: 2),
+          trailing: FavoriteButtonIcon(
+            venue.favorite ? Icons.favorite : Icons.favorite_outline,
+            onPressed: () => onPressed.call(venue.id, !venue.favorite),
+          ),
+          onTap: () {},
+        );
+      },
+      separatorBuilder: (context, index) => const CustomDivider(),
+      itemCount: venues.length,
+    );
+  }
+}
